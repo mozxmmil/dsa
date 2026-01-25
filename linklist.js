49ed542f49b8239ff0b2082d6c20954e010f0e67
mozxammil <mozxammil>
2026-01-25 19:49:10 +0530

    linkList

---
+++ linklist.js
@@ -0,0 +1,17 @@
+class Node {
+          constructor(data,next=null){
+                    this.data = data,
+                    this.next = next
+          }
+}
+class SingleLinkList {
+          constructor(){
+                    this.head = null
+          }
+
+          // here we are going to make the add data to the linkList 
+          addData(data){
+                    
+          }
+}
+
