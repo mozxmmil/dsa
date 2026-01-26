f3628c6da11e6dbbf2a025be4f17e0fb7ed3ed59
Mozammil『P A G A L 』 <143429276+mozxmmil@users.noreply.github.com>
2026-01-26 07:38:59 +0000

    update

---
+++ linklist.js
@@ -0,0 +1,20 @@
+// linklist 
+
+// lets make the node first 
+
+
+class Node () {
+
+    constructor(data,next=null) {
+        this.data = data
+        this.next = next 
+    }
+}
+
+class SingleLinkList (){
+    constructor() {
+        this.head = null
+    }
+
+
+}
