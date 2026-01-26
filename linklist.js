6936f42a831aea7df99d8a766423ca3d2759132c
mozxammil <mozxammil>
2026-01-26 13:11:17 +0530

    add data , and delete data work in linklist has been done

--- linklist.js
+++ linklist.js
@@ -1,17 +1,49 @@
 class Node {
-          constructor(data,next=null){
-                    this.data = data,
-                    this.next = next
-          }
+  constructor(data, next = null) {
+    ((this.data = data), (this.next = next));
+  }
 }
 class SingleLinkList {
-          constructor(){
-                    this.head = null
-          }
+  constructor() {
+    this.head = null;
+  }
 
-          // here we are going to make the add data to the linkList 
-          addData(data){
-                    
-          }
+  // here we are going to make the add data to the linkList
+  addData(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = newNode;
+  }
+
+  print() {
+    let current = this.head;
+    let print = [];
+    while (current !== null) {
+      print.push(current.data);
+      current = current.next;
+    }
+    console.log(print.join("-->"));
+  }
+
+  addAtBeginning(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
 }
 
+const link = new SingleLinkList();
+
+// console.log(link, "link");
+link.addData(4);
+link.addData(5);
+link.addAtBeginning(1);
+
+link.print();
