a0a3f708286135775b2c718eeef2ef9aa9420738
mozxammil <mozxammil>
2026-01-27 21:29:21 +0530

    in dubble link list all the delete operation has been done

--- dubbleLinkList.js
+++ dubbleLinkList.js
@@ -40,6 +40,49 @@ class DoubleLinkList {
     this.head = newNode;
   }
 
+  insertAtMiddle(data, position) {
+    let current = this.head;
+    while (current !== null && current.data !== position) {
+      current = current.next;
+    }
+    if (current === null) {
+      console.log("Position not found");
+      return;
+    }
+    let newNode = new Node(data);
+    newNode.next = current.next;
+    if (current.next !== null) {
+      current.next.prev = newNode;
+    }
+    current.next = newNode;
+    newNode.prev = current;
+  }
+
+  deleteNode(data) {
+    // first we need to find the that node is'nt first into the entire node
+
+    let currentNode = this.head;
+
+    if (this.head.data == data) {
+      this.head = currentNode.next;
+      this.head.prev = null;
+      return;
+    }
+
+    while (currentNode.next != null) {
+      if (currentNode.data == data) {
+        currentNode.prev.next = currentNode.next;
+        currentNode.next.prev = currentNode.prev;
+        return;
+      }
+      currentNode = currentNode.next;
+    }
+
+    if (currentNode.data == data) {
+      currentNode.prev.next = null;
+      return;
+    }
+  }
   print() {
     let temp = this.head;
     let result = [];
@@ -56,6 +99,9 @@ const dubbleLinkList = new DoubleLinkList();
 dubbleLinkList.insertAtEnd(10);
 dubbleLinkList.insertAtEnd(20);
 dubbleLinkList.insertAtEnd(30);
-dubbleLinkList.insertAtBeginning(1)
-dubbleLinkList.insertAtBeginning(5)
+dubbleLinkList.insertAtBeginning(1);
+dubbleLinkList.insertAtBeginning(5);
+dubbleLinkList.insertAtMiddle(15, 30);
+
+dubbleLinkList.deleteNode(5);
 dubbleLinkList.print(); // 10->20->30
