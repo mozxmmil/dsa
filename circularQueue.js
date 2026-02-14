336c2da539557b02113de7a96502faa501624f1c
mozxammil <mozxammil>
2026-02-14 22:20:29 +0530

    circular queue has been done

--- circularQueue.js
+++ circularQueue.js
@@ -1,10 +1,37 @@
 class Circular {
-  constructor() {
-    this.queue = [];
+  constructor(size) {
+    this.queue = new Array(size);
+
+    this.front = -1;
+    this.reare = -1;
+    this.size = size;
   }
 
   insert(data) {
-    this.queue.push(data);
+    // first we have to check kya queue full hai
+
+    if ((this.reare + 1) % this.size === this.front) {
+      console.log("Queue is full ");
+    } else if ((this.front = -1)) {
+      this.front = this.reare = -1;
+      this.insert[this.reare] = data;
+    } else {
+      this.reare = (this.reare + 1) % this.size;
+      this.insert[this.reare] = data;
+    }
   }
-  
+
+  delete() {
+    // chcking kya khali to nhi hia
+
+    if ((this.front = -1)) console.log("this is empty ");
+    else if (this.front == this.reare) {
+      this.front = this.reare = -1;
+      this.front  = (this.front + 1)% this.size
+    }
+    else {
+      this.front = (this.front+1) % this.size
+      
+    }
+  }
 }
