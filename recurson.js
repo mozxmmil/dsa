1f87aace43840ca01547f892d3b06a43f0e29ea5
mozxammil <mozxammil>
2026-02-16 23:09:48 +0530

    learned the factorial in recurson

--- recurson.js
+++ recurson.js
@@ -1,11 +1,17 @@
 // find the fibonachi number at the given position
 
-
 function fibonachi(n) {
-          if(n <=1 ) return n;
-          return fibonachi(n-1) + fibonachi(n-2);
-
-
+  if (n <= 1) return n;
+  return fibonachi(n - 1) + fibonachi(n - 2);
 }
 
-console.log(fibonachi(10))
+console.log(fibonachi(10));
+
+// factorial of a number
+
+function factorial(n) {
+  if (n == 0) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(5));
